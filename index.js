07ada593a012ea5701e0dbe162634385390241c2
AipBorges <mpbds@uic.es>
2021-11-01 15:33:50 +0100

    fixed router

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("pokemon.json");
+const router = jsonServer.router("list.json");
 const port = process.env.PORT || 4000;
 
 server.use(cors());
